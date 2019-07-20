e304b96757e8aabc7f8b83797d7bb4dfb7f5fdfb
teemak <bunth667@gmail.com>
2019-07-20 17:47:36 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	foo = () => "Bunthy";
+	render() {
+		const loading = true;
+		const name = "Tee Mak";
+
+		if (loading) {
+			return <h4>Loading...</h4>;
+		}
+
+		return (
+			<Fragment className="App">
+				<h1>HELLO {name}</h1>
+				<h2>React Practice</h2>
+				<p>{8 * 8}</p>
+				<p>{this.foo()}</p>
+			</Fragment>
+		);
+	}
 }
 
 export default App;
